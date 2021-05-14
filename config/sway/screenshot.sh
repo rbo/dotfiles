25c598ed6edd2ba6035d932e8121651e0af98361
Robert Bohne <robert.bohne@redhat.com>
2021-05-14 09:24:28 +0200

    Add screenshot

---
+++ config/sway/screenshot.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#set -x
+TEMP_DIR=$(mktemp -d)
+FILENAME=$(date "+%F-%H-%M-%S-%s.png")
+
+SOURCE=$1
+
+if [ "$1" == "wofi" ]; then
+    SOURCE=$(echo -e 'focuse_window\nselect\ndesktop' | wofi --show dmenu --width 150 --height 150)
+fi;
+
+case $SOURCE in
+
+    focused_window)
+        WINDOW=$(swaymsg -t get_tree | \
+            jq -r '..
+                | (.nodes? // empty)[]
+                | select(.focused==true)
+                | "\(.rect.x),\(.rect.y-.deco_rect.height) \(.rect.width)x\(.rect.height+.deco_rect.height)"
+            ')
+        grim -c -g "$WINDOW" ${TEMP_DIR}/${FILENAME}
+    ;;
+    desktop)
+        DESKTOP=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')
+        grim -o "$DESKTOP" ${TEMP_DIR}/${FILENAME}
+    ;;
+    focuse_window)
+        WINDOW=$(swaymsg -t get_tree | \
+            jq -r '..
+                | select(.pid? and .visible?)
+                | "\(.rect.x),\(.rect.y-.deco_rect.height) \(.rect.width)x\(.rect.height+.deco_rect.height)"
+            ' | slurp)
+        grim -g "$WINDOW" ${TEMP_DIR}/${FILENAME}
+    ;;
+    select)
+        grim -g "$(slurp)" ${TEMP_DIR}/${FILENAME}
+    ;;
+    *)
+        echo "Please run $0 [focused_window|focuse_window|select|desktop]"
+    ;;
+esac
+
+
+if [ -f ${TEMP_DIR}/${FILENAME} ] ; then
+    swappy -f ${TEMP_DIR}/${FILENAME}
+fi
+
+
+
