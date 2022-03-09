62f48318911b8c8b2d99fa8560d9dab3f8187b80
Robert Bohne <robert.bohne@redhat.com>
2022-03-09 14:36:55 +0100

    Use clockify-cli instead of curl

--- config/waybar/scripts/clockify.sh
+++ config/waybar/scripts/clockify.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 set -o pipefail
 CLI=/home/$USER/.config/waybar/scripts/clockify-cli
-export TOKEN=$( $CLI config -f json | jq -r '.token' )
-export USER_ID=$( $CLI config -f json | jq -r '.user.id' )
-export WORKSPACE=$( $CLI config -f json | jq -r '.workspace' )
-
 
 today_hours=$($CLI report today --duration-formatted)
 week_hours=$($CLI report this-week --duration-formatted)
 
-running=$(curl -s \
-    -H "content-type: application/json" \
-    -H "X-Api-Key: $TOKEN" \
-    -X GET \
-    https://api.clockify.me/api/v1/workspaces/${WORKSPACE}/user/${USER_ID}/time-entries?in-progress=true \
-| jq length )
-
-if [ "$running" == "0" ]; then
-  class="paused"
-else
-  class="running"
-fi
+$CLI show current -q > /dev/null && class=running || class=paused
 
 #tooltip="$(/home/$USER/.config/waybar/scripts/harvest |
 #    sed 's/\\/\\\\/g' |
