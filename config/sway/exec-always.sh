346633e35d004ef6df6960a773698c7401d8e92e
Robert Bohne <robert.bohne@redhat.com>
2022-06-17 12:26:19 +0200

    Switch to fedora 36

--- config/sway/exec-always.sh
+++ config/sway/exec-always.sh
@@ -13,11 +13,11 @@ export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
 
 swayidle -w \
   timeout 300  ~/.config/sway/lock \
-  timeout 330 'swaymsg "output * dpms off"' \
-  resume 'swaymsg "output * dpms on"' \
-  timeout 30 'if pgrep swaylock; then swaymsg "output * dpms off"; fi' \
-  resume 'if pgrep swaylock; then swaymsg "output * dpms on"; fi' \
   before-sleep ~/.config/sway/lock
+#  timeout 330 'swaymsg "output * dpms off"' \
+#  resume 'swaymsg "output * dpms on"' \
+#  timeout 30 'if pgrep swaylock; then swaymsg "output * dpms off"; fi' \
+#  resume 'if pgrep swaylock; then swaymsg "output * dpms on"; fi' \
 
 #/usr/libexec/xdg-desktop-portal -r &
 #/usr/libexec/xdg-desktop-portal-wlr -r &
