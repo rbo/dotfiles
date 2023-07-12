810fcb8b322c40b113b034616e1f589b037fbd24
Robert Bohne <robert.bohne@redhat.com>
2023-07-12 10:16:59 +0200

    Update tmux powerline

--- config/tmux-powerline/themes/rbohne.sh
+++ config/tmux-powerline/themes/rbohne.sh
@@ -79,6 +79,7 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		"tmux_session_info 148 234" \
 		"hostname 33 0" \
+		"pwd 89 211" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
@@ -94,7 +95,7 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		#"pwd 89 211" \
 		#"macos_notification_count 29 255" \
 		#"mailcount 9 255" \
 		# "now_playing 234 37" \
